f6b9fed7f58ff240bb4fd96acad959ac28415f27
Bipin Lamichhane <bipinlamichhane36@gmail.com>
2026-03-31 10:39:38 +0100

    Completed

--- main.tf
+++ main.tf
@@ -1,4 +1,91 @@
-resource "aws_instance" "myec2instance" {
-  ami = "ami-0ff8a91507f77f867"
-  instance_type = "t3.micro"
+resource "aws_s3_bucket" "my-s3" {
+  bucket = var.bucket-name
+}
+
+resource "aws_s3_bucket_public_access_block" "access-block" {
+  bucket = aws_s3_bucket.my-s3.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_cloudfront_origin_access_control" "my-oac" {
+  name                              = "example-oac"
+  description                       = "OAC for S3 static website"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.my-s3.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.my-oac.id
+    origin_id                = "S3Origin"
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3Origin"
+
+    forwarded_values {
+      query_string = false
+      cookies { forward = "none" }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  restrictions {
+    geo_restriction { restriction_type = "none" }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+resource "aws_s3_bucket_policy" "cdn-oac-policy" {
+  bucket = aws_s3_bucket.my-s3.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AllowCloudFrontServicePrincipalReadOnly"
+        Effect    = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.my-s3.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.s3_distribution.arn
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_s3_object" "upload_website_files" {
+  for_each = fileset("${path.module}/www/", "**/*")
+
+  bucket       = aws_s3_bucket.my-s3.id
+  key          = each.value
+  source       = "${path.module}/www/${each.value}"
+  
+  content_type = lookup({
+    "html" = "text/html",
+    "css"  = "text/css",
+    "js"   = "application/javascript",
+    "png"  = "image/png",
+    "jpg"  = "image/jpeg"
+  }, split(".", each.value)[length(split(".", each.value)) - 1], "text/plain")
 }
